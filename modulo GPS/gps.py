9fd9c17b55e7ef0e5ffa2faa3de461e7f6a3f0d4
CarlaEnricci <carlamicaela2004@gmail.com>
2023-11-06 09:33:01 -0300

    style: quitar comentarios

--- modulo GPS/gps.py
+++ modulo GPS/gps.py
@@ -2,11 +2,7 @@ from machine import Pin, UART
 import utime
 from micropyGPS import MicropyGPS
  
-# Initialize I2C and OLED display
-# i2c = I2C(1, sda=Pin(14), scl=Pin(15), freq=400000)
-# oled = SSD1306_I2C(128, 64, i2c)
- 
-# Initialize GPS module
+# Inicializar modulo GPS
 gps_module = UART(1, baudrate=9600, tx=Pin(8), rx=Pin(9))
 time_zone = -3
 gps = MicropyGPS(time_zone)
@@ -27,7 +23,6 @@ def convert_coordinates(sections):
     data = '{0:.6f}'.format(data)  # 6 decimal places
     return str(data)
  
- 
 while True:
     length = gps_module.any()
     if length > 0:
@@ -39,20 +34,8 @@ while True:
     longitude = convert_coordinates(gps.longitude)
  
     if latitude is None or longitude is None:
-#         oled.fill(0)
          print("Data unavailable")
          print("No coordinates")
-#         oled.show()
          continue
- 
-
-    
-#     oled.fill(0)
-#     oled.text('Satellites: ' + str(gps.satellites_in_use), 10, 0)
-#     oled.text('Lat: ' + latitude, 0, 18)
     print('Lat: ' + latitude)
-#     oled.text('Lon: ' + longitude, 0, 36)
     print('Lon: ' + longitude)
- 
-#     oled.show()
-        
