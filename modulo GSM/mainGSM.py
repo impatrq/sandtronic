b2ab00d3bb551050371287b09d754a35aaa194fc
CarlaEnricci <carlamicaela2004@gmail.com>
2023-11-08 20:08:09 -0300

    feat: agregar codigo funcional
    testeo de capacidad de envio del mensaje

--- modulo GSM/mainGSM.py
+++ modulo GSM/mainGSM.py
@@ -1,4 +1,13 @@
 from machine import UART, Pin
-uart1 = UART(1, baudrate=9600, rx=Pin(13), tx=Pin(12))
+import time
+gsm = UART(0, baudrate=9600, rx=Pin(13), tx=Pin(12))
 
 #uart1.read()
+gsm.write('AT+CMGS="+6xxxxxxxxx68"'+'\r\n')
+rcv = gsm.read()
+print(rcv)
+time.sleep(1)
+
+gsm.write('HOLA'+'\r\n')
+rcv = gsm.read()
+print(rcv)
