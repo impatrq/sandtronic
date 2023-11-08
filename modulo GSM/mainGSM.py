33d69e882220895965e7725f38de7212e498aa28
CarlaEnricci <carlamicaela2004@gmail.com>
2023-11-08 20:20:42 -0300

    feat: agregar libreria
    libreria: https://github.com/Ircama/raspberry-pi-sim800l-gsm-module

--- modulo GSM/mainGSM.py
+++ modulo GSM/mainGSM.py
@@ -3,7 +3,10 @@ import time
 gsm = UART(0, baudrate=9600, rx=Pin(13), tx=Pin(12))
 
 #uart1.read()
-gsm.write('AT+CMGS="+6xxxxxxxxx68"'+'\r\n')
+gsm.write("AT+CMGF=1\r") # set to text mode
+time.sleep(1)
+
+gsm.write('AT+CMGS="+5491128857582"'+'\r\n')
 rcv = gsm.read()
 print(rcv)
 time.sleep(1)
