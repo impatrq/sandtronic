80b4b84d5f2ecf8a03311bb8f808bebe93bff22a
mate2004 <mateo.zanichelli@hotmail.com>
2023-11-10 10:35:08 -0300

    feature: implement code as objects
    Convierto setup del velocimetro y del pwm del estator en clases

--- Python/main.py
+++ Python/main.py
@@ -8,54 +8,60 @@ from machine import Pin, I2C, PWM
 
 ### parametros dedicados del pwm estator 
 
-def map(var, low_min, low_max, high_min, high_max):
-    low_range = low_max - low_min
-    per = var / low_range
-    high_range = high_max - high_min
-    result = high_range * per
-    return int(result)
+class stator(): 
+    def __init__(self):               
+        self.pwm1 = PWM(Pin(22)) # pwm del estator
+        self.pwm1.freq(2800)
 
-pwm1 = PWM(Pin(22)) # pwm del estator
-pwm1.freq(2800)
+        self.i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000) # direc i2c del adc externo
+        self.adc = ads1115.ADS1115(self.i2c, 72, 0) # adc externo
 
-i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=400000) # direc i2c del adc externo
-adc = ads1115.ADS1115(i2c, 72, 0) # adc externo
+    def map(self, var, low_min, low_max, high_min, high_max):
+        low_range = low_max - low_min
+        per = var / low_range
+        high_range = high_max - high_min
+        result = high_range * per
+        return int(result)
+
 
 ### parametros y funciones dedicados del velocimetro
 
-servo_pin = 13
+class velocimeter():
+    def __init__(self):   
+        self.servo_pin = 13 # Pin del servo del velocimetro
+        self.sensor_pin = 20 # Pin del sensor 
 
-pwm2 = machine.PWM(machine.Pin(servo_pin))
-pwm2.freq(50)
+        self.pwm2 = machine.PWM(machine.Pin(self.servo_pin))
+        self.pwm2.freq(50)
 
-sensor_pin = machine.Pin(20, machine.Pin.IN)
+        self.sensor = machine.Pin(self.sensor_pin, machine.Pin.IN) # sensor de pulsos de motor
 
-pulse_count = 0
-speed = 0.0
-diametro= 0.55
-circunferencia= 0.55 * 3.14
-relacion = 8.9
-cant_imanes = 4
+        self.pulse_count = 0
+        self.speed = 0.0
+        self.diametro= 0.55
+        self.circunferencia= 0.55 * 3.14
+        self.relacion = 8.9
+        self.cant_imanes = 4
 
+        self.servo_duty_0 = 8000
+        self.servo_duty_180 = 1333
 
-servo_duty_0 = 8000
-servo_duty_180 = 1333
+        self.duty = self.servo_duty_0
 
-duty = servo_duty_0
+        self.m = (1333-8000) / 80
 
-m = (1333-8000) / 80
+    def calculate_speed(self):
+        #global pulse_count, speed, duty
+        self.speed = self.pulse_count * 10 / (self.relacion * self.cant_imanes) * self.circunferencia * 3.6
+        self.duty = int (self.m * self.speed + self.servo_duty_0)
+        self.pulse_count = 0
+        self.speed_str = "velocidad: {:.1f} km?h" .format(self.speed)
+        return self.speed_str
 
-def calculate_speed(t):
-    global pulse_count, speed, duty
-    speed = pulse_count * 10 / (relacion * cant_imanes) * circunferencia * 3.6
-    duty = int (m * speed + servo_duty_0)
-    pulse_count = 0
-    print("velocidad: {:.1f} km?h" .format(speed))
-    
-    
-def on_pulse(pin):
-    global pulse_count
-    pulse_count += 1
+    def on_pulse(self):
+        #global pulse_count
+        self.pulse_count += 1
+        return self.pulse_count
 
 ###
 
@@ -72,11 +78,13 @@ def pwm_stator():
     utime.sleep(0.05)
 
 def velocimetro():
-    print (pulse_count)
-    print (speed)
+    print(calculate_speed())
+    print (on_pulse())
     pwm2.duty_u16(duty)
 
 def main():
+    stator_setup()
+    velocimeter_setup()
     while True: 
         try: # safeguard por si se llena la queue de threads, evito crasheos
             _thread.start_new_thread(pwm_stator)
