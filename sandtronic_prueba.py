59d70c8892f45a8c05837c8e7195dce4d1b73d61
NicolasMorellato <92580376+NicolasMorellato@users.noreply.github.com>
2023-11-23 14:48:12 -0800

    fix: modos y estados de sandtronic

--- sandtronic_prueba.py
+++ sandtronic_prueba.py
@@ -6,3 +6,11 @@ class Sandtronic:
     
     VELOCIDAD_MAXIMA = 70 # Esta en km/h
    
+   def __init__(self, velocidad):
+        self.modo = ECO
+        self.reversa = False
+        self.encendido = False
+        self.velocidad = 0
+        self.velocidad_max = velocidad
+        self.velocimetro = classvelocimetro.Velocimetro()
+        
