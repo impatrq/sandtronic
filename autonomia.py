e6a240ba4598e56791a7673443d6cf7dab2b6bdc
NicolasMorellato <92580376+NicolasMorellato@users.noreply.github.com>
2023-11-24 09:16:34 -0800

    fix: añadir tiempoi

--- autonomia.py
+++ autonomia.py
@@ -24,6 +24,16 @@ def medidor_autonomia_init():
     valor_adc = adc.read_u16()  # Lee el valor del ADC en formato de 16 bits
     # Convierte el valor del ADC a voltaje
     voltaje = valor_adc * 3.3 / 65535  # 65535 es el valor máximo de ADC en 16 bits, 3.3 es la referencia de voltaje
+     corriente = voltaje / (15 * 0.00025) #a partir de voltaje medido en el adc se saca la corriente 
+    consumo_promedio += corriente 
+    index += 1
+       
+    tiempo2 = time.ticks_ms()
     
+    if (tiempo2 - tiempo1)/1000 >= 60:
+        consumo_promedio = consumo_promedio / index
+        index = 0
+        tiempo1 = time.ticks_ms()
+        return consumo_promedio 
     
     
