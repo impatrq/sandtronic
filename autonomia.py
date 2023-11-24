962ab5b48c6d4fc0ab3bf696731d919cb7718185
NicolasMorellato <92580376+NicolasMorellato@users.noreply.github.com>
2023-11-24 09:15:06 -0800

    fix: añadir adc valor voltaje

--- autonomia.py
+++ autonomia.py
@@ -15,3 +15,12 @@ def medidor_autonomia_init():
     adc = ADC(0)  # Usamos ADC(0) para el pin GP26
     tiempo1 = time.ticks_ms() #Toma tiempo actual en ms del procesador    
     
+    def medir_autonomia():
+    global consumo_promedio
+    
+    if index == 0:
+        consumo_promedio = 0
+    # Lee el valor del ADC
+    valor_adc = adc.read_u16()  # Lee el valor del ADC en formato de 16 bits
+    # Convierte el valor del ADC a voltaje
+    voltaje = valor_adc * 3.3 / 65535  # 65535 es el valor máximo de ADC en 16 bits, 3.3 es la referencia de voltaje
