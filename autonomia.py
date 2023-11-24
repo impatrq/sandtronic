96bdcc48d798f0e53850979b1943989b4b57ea69
NicolasMorellato <92580376+NicolasMorellato@users.noreply.github.com>
2023-11-24 09:10:19 -0800

    fix:añadir def medidor autonomia

--- autonomia.py
+++ autonomia.py
@@ -8,3 +8,10 @@ indexminutos = 0
 
 adc = 0
 consumo_promedio = 0
+
+def medidor_autonomia_init():
+    global adc
+    # Configura el ADC
+    adc = ADC(0)  # Usamos ADC(0) para el pin GP26
+    tiempo1 = time.ticks_ms() #Toma tiempo actual en ms del procesador    
+    
